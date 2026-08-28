0285b6a7777e1141be8a3837a2bbf3b5a9cd4a63
wordtoolslab <chrislo961@gmail.com>
2026-08-28 08:52:53 +0200

    Load phase 41 enhancements on solver routes

--- site-config.js
+++ site-config.js
@@ -3,3 +3,16 @@ window.LETTERTOOLKIT_CONFIG = {
   adsensePublisherId: "",
   searchConsoleVerification: ""
 };
+
+(function loadRouteEnhancements(){
+  const route=location.pathname.replace(/\/$/,'');
+  if(route!=='/crossword-solver'&&route!=='/word-finder')return;
+  const css=document.createElement('link');
+  css.rel='stylesheet';
+  css.href='/phase41-crossword-wordfinder.css?v=1';
+  document.head.appendChild(css);
+  const script=document.createElement('script');
+  script.src='/phase41-crossword-wordfinder.js?v=1';
+  script.defer=true;
+  document.head.appendChild(script);
+})();
